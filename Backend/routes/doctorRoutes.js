d08ece8fe3afcdc08bcafd90a6f980b7850bbd05
venkat88676 <venkat88676@gmail.com>
2023-06-16 17:20:37 +0530

    doctor-backend

---
+++ Backend/routes/doctorRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const {DocModel} = require("../model/doctor")
+const docRouter = express.Router();
+
+docRouter.get("/", async (req, res) => {
+    try{
+        console.log("inside")
+        let docDetails= await DocModel.find();
+        res.status(200).send(docDetails)
+    }catch(err){
+        res.status(400).send(err)
+    }
+});
+
+docRouter.post("/add",async(req,res)=>{
+    const {name,specialist,img,bio,email,experience,rating}=req.body
+    const product=new DocModel({name,specialist,img,bio,email,experience,rating})
+    try{
+        await product.save()
+         res.status(200).send({"msg":"added"})
+    }catch(err){
+        res.status(400).send(err)
+    }    
+})
+
+
+  module.exports={docRouter}
