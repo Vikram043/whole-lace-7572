50c514bb244979e99d6f85a4be9d9a7d5f9d67d1
0AvinashMohanDev1 <avinashmohandev@gmail.com>
2023-06-16 23:09:07 +0530

    front-end code

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
-
+// import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import '../Style/CssFooter.css'
 export default function Footer() {
+  let mediaData=[{
+    img:"https://lh3.googleusercontent.com/PxlTITdvAq-gLp6Pi_dcQZh6dvw6T4HsK7sH4gJ5fTFo0sWkVR0i8OI-H6CHYAt0nmwYwGespDuhfPDehQXQt9dClA=w640-h400-e365-rj-sc0x00ffffff",
+    link:"https://www.youtube.com/@PetCareujjwal"
+  },
+  {
+    img:"https://robots.net/wp-content/uploads/2020/03/Photo-by-Tumisu-1-1-600x595.jpg",
+    link:"https://www.instagram.com/zoetispetcare/"
+  },
+  {
+    img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiGr0Vsh8v462eX_DiahmbC58_-CgK-MwASA&usqp=CAU",
+    link:"https://www.facebook.com/groups/692556975452740"
+  },
+  {
+    img:"",
+    link:""
+  }]
   return (
-    <div>
-      
+    <div id='footerDiv'>
+      <div>
+        <img className='logo' src="https://www.linkpicture.com/q/logo-no-background.png" alt="" />
+        <h2>About us:</h2>
+        <p>We are the pet care. We take care of pets, we solves the pets prolems. We love to save pets and help them to recover from any 
+          kind of Disease.  
+        </p>
+        <h2>Services</h2>
+        <h2>Partner with Us</h2>
+      </div>
+      <div>
+        {
+          mediaData.map((ele)=>{
+            return (
+              <div>
+                <button>
+                  <img src={ele.img} alt="" />
+                  <link rel="stylesheet" href={ele.link} />
+                </button>
+              </div>
+            )
+          })
+        }
+      </div>
+      <div>
+        <h2>Contact us:</h2>
+        <p>Email:</p>
+        <p>      caretaker@gmail.com</p>
+        <p>Phone:</p>
+        <p>      098776543210</p>
+        <p>      098776543211</p>
+        
+      </div>
     </div>
   )
 }
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,116 @@
 import React from 'react'
 
-export default function Home() {
+import Navbar from '../Components/Navbar'
+import Footer from '../Components/Footer'
+import Pet from './Pet'
+import '../Style/CssHome.css'
+import {Link} from 'react-router-dom'
+export default function home() {
+  let doctorData=[{
+    id:1,
+    name:"DR. Avinash Mohan Dev",
+    profession:"Animal expert",
+    img:"https://media.licdn.com/dms/image/D4D03AQFRKeCpF8J1fQ/profile-displayphoto-shrink_800_800/0/1683776037836?e=1692230400&v=beta&t=eOLnvulAaOtASTCuNzk6PjbDAtUdF2k0BpU0Y91xzvM"
+  },{
+    id:2,
+    name:"DR. Vikram",
+    profession:"Dog expert",
+    img:"https://media.licdn.com/dms/image/D4D03AQFRKeCpF8J1fQ/profile-displayphoto-shrink_800_800/0/1683776037836?e=1692230400&v=beta&t=eOLnvulAaOtASTCuNzk6PjbDAtUdF2k0BpU0Y91xzvM"
+  },
+  {
+    id:3,
+    name:"Dr. Kamlesh Das",
+    profession:"Cat expert",
+    img:"https://media.licdn.com/dms/image/D4D03AQFRKeCpF8J1fQ/profile-displayphoto-shrink_800_800/0/1683776037836?e=1692230400&v=beta&t=eOLnvulAaOtASTCuNzk6PjbDAtUdF2k0BpU0Y91xzvM"
+  },
+  {
+    id:4,
+    name:"Dr. Rohit",
+    profession:"Fish expert",
+    img:"https://media.licdn.com/dms/image/D4D03AQFRKeCpF8J1fQ/profile-displayphoto-shrink_800_800/0/1683776037836?e=1692230400&v=beta&t=eOLnvulAaOtASTCuNzk6PjbDAtUdF2k0BpU0Y91xzvM"
+  },
+  {
+    id:5,
+    name:"Dr. Venkatesh",
+    profession:"Bird expert",
+    img:"https://media.licdn.com/dms/image/D4D03AQFRKeCpF8J1fQ/profile-displayphoto-shrink_800_800/0/1683776037836?e=1692230400&v=beta&t=eOLnvulAaOtASTCuNzk6PjbDAtUdF2k0BpU0Y91xzvM"
+  }]
+
+  let petData=[
+    {
+      id:1,
+      category:"Dog",
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlLzFh8163pz_ToeBIAunf7A9MJXzfok_rTvlgSWpvFSnxNGaheFkQ4tNa2Pok8KFUvPo&usqp=CAU"
+    },
+    {
+      id:2,
+      category:"Cat",
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl6G5bsRs0dhtS3TVGclhjkwPxBTY9RXO8qw&usqp=CAU"
+    },
+    {
+      id:3,
+      category:"Fish",
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ640hAVWwnZHzNqQ4GuKo-jPhv-9gXKxeBzg&usqp=CAU"
+    },
+    {
+      id:4,
+      category:"Birds",
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdkvsvlzJ6NE5FXVC2k6m9_VGxFod6nvmAmA&usqp=CAU"
+    },
+    {
+      id:5,
+      category:"Rabbit",
+      img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNtofi4iiIQLX1AROZryO842Uazya9EMxVtA&usqp=CAU"
+    }
+  ]
+
   return (
     <div>
+      <Navbar />
+  {/* Banner Image */}
+      <img id='banner' src="https://img.freepik.com/free-psd/horizontal-banner-pet-care-with-female-veterinarian-yorkshire-terrier-dog_23-2148911274.jpg?size=626&ext=jpg&ga=GA1.1.227996513.1683780617&semt=ais" alt="banner" />
+  {/* Doctors details */}
+      <div>
+        <h2>Doctors Details</h2>
+      </div>
+      <div id='docData-div'>
+      {
+        doctorData.map((ele)=>{
+          return(
+            <div>
+            <img src={ele.img} alt="404" />
+            <p>{ele.name}</p>
+            <p>{ele.profession}</p>
+          </div>
+          )
+        })
+      }
+      </div>
+{/* Pets details */}
+      <div>
+        <h2>Pets Details</h2>
+      </div>
+      <div id='petData-div'>
+      {
+        petData.map((ele)=>{
+          return (
+            <div>
+            <img src={ele.img} alt="" />
+            <p>{ele.category}</p>
+            <button  >check out</button>
+            <Link to={`/Pet/${ele.id}`}>
+            <button>check out</button>
+            </Link>
+          </div>
+          )
+        })
+      }
+      {/* onClick={
+              ()=><Pet id={ele.id}/>
+            }> */}
+      </div>
       
+      <Footer />
     </div>
   )
 }
