27b8f54083b4577104d9f155e0d919ed55719c6d
Rohit2216 <chauhanrohit716@gmail.com>
2023-06-17 00:24:23 +0530

    booking appointment frontend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 // import Dashboard from './pages/Dashboard'
 // import Layout from './components/shared/Layout.jsx'
 // import Products from './pages/Products'
 
+import {Client} from './Pages/Client';
+
 function App() {
     return (
         <Router>
+            
             {/* <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Dashboard />} />
@@ -13,8 +18,12 @@ function App() {
                 </Route>
                 <Route path="/register" element={<div>Register</div>} />
             </Routes> */}
+
+            <Routes>
+            <Route path="/client" element={<Client />} />
+            </Routes>
         </Router>
     )
 }
 
-export default App
+export default App;
